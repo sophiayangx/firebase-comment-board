3f7d9572ce5c6094e32240139e8e0d9fc68ef4f3
sophiayangx <sopyang@nuevaschool.org>
2016-12-21 15:15:55 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-console.log("hi")
-
 var commentInput = $("#comment-input")
-var commentDisplay = $("#comment-display")
-//
-//commentInput.keydown(function(e) {
-//  if (e.keyCode == "13") {
-//    addListItem("comments", commentInput.val());
-//  }
-//})
-//
-//onNewListItem("comments", function(newComment) {
-//  commentDisplay.append("<div>" + newComment + "</div>")
-//})
+var commentDisplay = $(".comment-display")
 
-commentInput.keydown(function(e) {	
-  if (e.keyCode == "13") {
-    var valueToSave = commentInput.val();
-    addListItem("sophiasList", valueToSave);
+commentInput.keydown(function(e) {
+  if (e.which === 13) {
+    addListItem("comments", commentInput.val());
   }
 })
 
-onNewListItem("sophiasList", function(value) {
-  commentDisplay.append("<div>" + value + "</div>")
-}) 
+onNewListItem("comments", function(newComment) {
+  commentDisplay.append("<div>" + newComment + "</div>")
+})
